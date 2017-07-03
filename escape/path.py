48a5dfaf7f543d2540175065a6fb578018b628e8
Bart Spaans <bart.spaans@gmail.com>
2017-07-03 15:28:03 +0100

    Add 'add_binary_to_path' function

--- escape/path.py
+++ escape/path.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 def is_binary_on_path(binary, path_string = None):
     if path_string is None:
@@ -8,6 +9,18 @@ def is_binary_on_path(binary, path_string = None):
         p = os.path.join(d, binary)
         if os.path.isfile(p) and os.access(p, os.X_OK):
             return True
+    return False
+
+def add_binary_to_path(binary_location):
+    path = get_path()
+    for p in path.split(":"):
+        target = os.path.join(p, os.path.basename(binary_location))
+        try:
+            shutil.move(binary_location, target)
+            print "Copied", binary_location, "to", target
+            return True
+        except:
+            print "WARN: Couldn't copy", binary_location, "to", target
     return False
 
 def get_path():
--- escape/test_path.py
+++ escape/test_path.py
@@ -1,4 +1,6 @@
 import unittest
+import os
+import shutil
 from escape import path
 
 class TestInputReaders(unittest.TestCase):
@@ -11,6 +13,25 @@ class TestInputReaders(unittest.TestCase):
         }
         for test, expected in cases.iteritems():
             self.assertEqual(path.is_binary_on_path(test), expected, test)
+
+    def test_add_binary_to_path(self):
+        previousPath = os.environ["PATH"]
+        os.environ["PATH"] = "test_path"
+        os.mkdir("test_path")
+        with open("test_file.txt", "w") as f:
+            f.write("test")
+        self.assertFalse(os.path.exists("test_path/test_file.txt"))
+        self.assertTrue(path.add_binary_to_path("test_file.txt"))
+        self.assertTrue(os.path.exists("test_path/test_file.txt"))
+        shutil.rmtree("test_path")
+        os.environ["PATH"] = previousPath
+
+    def test_get_path(self):
+        previousPath = os.environ["PATH"]
+        os.environ["PATH"] = "yo"
+        self.assertEqual(path.get_path(), "yo")
+        os.environ["PATH"] = previousPath
+
 
     def test_parse_path_string(self):
         cases = {
